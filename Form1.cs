362ab174b4e6b7fbfc9e05d391eb95c59ac01a82
mustafabaihaqi <mustafabaihaqi@gmail.com>
2021-04-29 09:50:01 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CobaDatabase
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection koneksi = new SqlConnection(@"Data Source=IRONBOY;Initial Catalog=CekDatabase;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi.Open();
+                MessageBox.Show("Koneksi Berhasil");
+                koneksi.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
